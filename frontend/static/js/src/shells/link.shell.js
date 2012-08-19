fb57f089ef11dbe31286dbff94199656402b0aed
jbenet <juan@benet.ai>
2012-08-19 06:35:16 -0700

    temporary LinkRegExp (gruber's fails).

--- frontend/static/js/src/shells/link.shell.js
+++ frontend/static/js/src/shells/link.shell.js
@@ -1,22 +1,33 @@
 
 // LinkRegExp -- Regular
 
-var LinkRegExp = RegExp(''
-  // word boundaries
-  + '\b('
+// var LinkRegExp = RegExp(''
+//   + '\b'                      // words
+//   + '('                       // Capture 1: entire matched URL
+//   +   '(?:'
+//   +     'https?://'                   // http or https protocol
+//   +     '|'                           // or
+//   +     'www\d{0,3}[.]'               // "www.", "www1.", "www2." … "www999."
+//   +     '|'                           // or
+//   +     '[a-z0-9.\-]+[.][a-z]{2,4}/'  // domain name followed by a slash
+//   +   ')'
+//   +   '(?:'                       // One or more:
+//   +     '[^\s()<>]+'                  // Run of non-space, non-()<>
+//   +     '|'                           // or
+//   +     '\(([^\s()<>]+|(\([^\s()<>]+\)))*\)' // balanced parens, up to 2 levels
+//   +   ')+'
+//   +   '(?:'                       // End with:
+//   +     '\(([^\s()<>]+|(\([^\s()<>]+\)))*\)'  // balanced parens, up to 2 levels
+//   +     '|'                               //   or
+//   +     '[^\s`!()\[\]{};:\'".,<>?«»“”‘’]'  // not these punct chars
+//   +   ')'
+//   + ')',
+//   'i'
+// );
+//
 
-    // protocol and domain
-    + '(?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)'
-
-    // TODO: ???
-    + '(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+'
-
-    // Filepath + Query String
-    + '(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’])'
-
-  + ')',
-  'i'
-);
+var LinkRegExp =
+  RegExp('http://[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]', 'i');
 
 
 // LinkValidationInterface -- for determining whether links match.
