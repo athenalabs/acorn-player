3fdc5ab38c4047ac3297a3971a9d3c77e4caf3ad
jbenet <juan@benet.ai>
2012-09-08 09:27:56 -0700

    added server.py, a very simple server.

---
+++ server.py
@@ -0,0 +1,25 @@
+import BaseHTTPServer
+import SimpleHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+class PlayerHandler(SimpleHTTPRequestHandler):
+
+  def do_GET(self):
+
+    # / should return player.html.
+    if self.path == '/':
+      self.path = '/player.html'
+
+    # player/ requests should return player.html.
+    if self.path.startswith('/player/'):
+      self.path = '/player.html'
+
+    return SimpleHTTPRequestHandler.do_GET(self)
+
+
+def test(HandlerClass = PlayerHandler,
+         ServerClass = BaseHTTPServer.HTTPServer):
+    BaseHTTPServer.test(HandlerClass, ServerClass)
+
+if __name__ == '__main__':
+    test()
