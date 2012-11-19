70c3fc4ec36a3a0102b3b16c4fb5292e680ed6db
Daniel Windham <dewindham@gmail.com>
2012-11-19 02:54:15 -0800

    acorn: bugfix util.clone

--- js/src/acorn.js
+++ js/src/acorn.js
@@ -434,7 +434,7 @@
     if (!isObject(obj)) return obj;
     return isArray(obj) ? obj.slice() : extend({}, obj);
   };
-  acorn.util.clone;
+  acorn.util.clone = clone;
 
 
   // Originally from backbone.js 0.9.1:
