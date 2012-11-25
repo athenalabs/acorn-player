d9d2868c3078624bfa8f0d96e7e2fe391a88b919
Daniel Windham <dewindham@gmail.com>
2012-11-24 17:05:22 -0800

    libs/jqueryUI: update from 1.8.21 to 1.9.1
    remove `.slider('destroy')` call: jQueryUI now throws an error for calls
    to uninitialized widgets (http://bugs.jqueryui.com/ticket/5972)

--- js/src/shells/videolink.shell.js
+++ js/src/shells/videolink.shell.js
@@ -253,7 +253,6 @@ VideoLinkShell.EditView = LinkShell.EditView.extend({
 
     // setup slider
     self = this;
-    this.$el.find('#slider').slider('destroy');
     this.$el.find('#slider').slider({
       min: 0,
       max: max,
